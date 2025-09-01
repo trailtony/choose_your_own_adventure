f88a517805ad9d6130d352da47795b2c0ef9b15a
trailtony <still_running_uphill@protonmail.com>
2025-09-01 22:53:11 +0100

    Added import schema to StoryGenerator.jsx and a layer of abstraction through util.js

---
+++ frontend/src/components/StoryGenerator.jsx
@@ -0,0 +1,6 @@
+import {useState, useEffect} from "react"
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
+import ThemeInput from "./ThemeInput.jsx";
+import LoadingStatus from "./LoadingStatus.jsx";
+import {API_BASE_URL} from "../util.js";
--- frontend/src/components/StoryLoader.jsx
+++ frontend/src/components/StoryLoader.jsx
@@ -3,9 +3,8 @@ import {useParams, useNavigate} from "react-router-dom";
 import axios from 'axios';
 import LoadingStatus from "./LoadingStatus.jsx";
 import StoryGame from "./StoryGame.jsx";
+import { API_BASE_URL } from '../util.js';
 
-
-const API_BASE_URL = "/api"
 
 function StoryLoader() {
     const {id} = useParams();
